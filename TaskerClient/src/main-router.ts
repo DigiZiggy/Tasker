1d96f55debe11336977c3fb55a5a2227cc95fc90
Sigrid Narep <sigridnarep@Sigrids-MacBook-Air-2.local>
2019-03-09 11:47:01 +0200

    Added Tasker aurelia Client

---
+++ TaskerClient/src/main-router.ts
@@ -0,0 +1,26 @@
+import {PLATFORM,LogManager} from "aurelia-framework";
+import {RouterConfiguration, Router} from "aurelia-router";
+
+export var log = LogManager.getLogger('MainRouter');
+
+export class MainRouter {
+  
+  public router : Router;
+  
+  constructor() {
+    log.debug('constructor');
+  }
+
+  configureRouter(config: RouterConfiguration, router: Router):void {
+    log.debug('configureRouter');
+
+    this.router = router;
+    config.title = "Tasker App -Aurelia";
+    config.map(
+      [
+        {route: ['', 'home'], name: 'home', moduelId: PLATFORM.moduleName('home'), nav: true, title: 'Home'}
+      ]
+      
+    );
+  }
+}
