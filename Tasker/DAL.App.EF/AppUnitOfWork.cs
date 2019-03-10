23c9730eff217362192bf40318c294d6df54c1bc
Sigrid Narep <sigridnarep@Sigrids-MacBook-Air-2.local>
2019-03-10 14:52:09 +0200

    UOW better implementation

--- Tasker/DAL.App.EF/AppUnitOfWork.cs
+++ Tasker/DAL.App.EF/AppUnitOfWork.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Contracts.DAL.App;
 using Contracts.DAL.App.Repositories;
@@ -11,49 +13,89 @@ namespace DAL.App.EF
     {
         private readonly AppDbContext _appDbContext;
         
-        public IAddressRepository Addresses => new AddressRepository(_appDbContext);
-        public ICityRepository Cities => new CityRepository(_appDbContext);
-        public ICountryRepository Countries => new CountryRepository(_appDbContext);
-        public IHourlyRateRepository HourlyRates => new HourlyRateRepository(_appDbContext);
-        public IIdentificationRepository Identifications => new IdentificationRepository(_appDbContext);
-        public IIdentificationTypeRepository IdentificationTypes => new IdentificationTypeRepository(_appDbContext);
-        public IInvoiceRepository Invoices => new InvoiceRepository(_appDbContext);
-        public IInvoiceLineRepository InvoiceLines => new InvoiceLineRepository(_appDbContext);
-        public IMeansOfPaymentRepository MeansOfPayments => new MeansOfPaymentRepository(_appDbContext);
-        public IPaymentRepository Payments => new PaymentRepository(_appDbContext);
-        public IPriceListRepository PriceLists => new PriceListRepository(_appDbContext);
-        public IPriceRepository Prices => new PriceRepository(_appDbContext);
-        public IReviewRepository Reviews => new ReviewRepository(_appDbContext);
-        public ISkillRepository Skills => new SkillRepository(_appDbContext);
-        public ITaskRepository Tasks => new TaskRepository(_appDbContext);
-        public ITaskTypeRepository TaskTypes => new TaskTypeRepository(_appDbContext);
-        public IUserGroupRepository UserGroups => new UserGroupRepository(_appDbContext);
-        public IUserInGroupRepository UserInGroups => new UserInGroupRepository(_appDbContext);
-        public IUserOnAddressRepository UserOnAddresses => new UserOnAddressRepository(_appDbContext);
-        public IUserOnTaskRepository UserOnTasks => new UserOnTaskRepository(_appDbContext);
-        public IUserRepository Users => new UserRepository(_appDbContext);
-        public IUserSkillRepository UserSills => new UserSkillRepository(_appDbContext);
-        public IUserTypeRepository UserTypes => new UserTypeRepository(_appDbContext);
+        private readonly Dictionary<Type, object> _repositoryCache = new Dictionary<Type, object>();
+
+        public IAddressRepository Addresses =>
+            GetOrCreateRepository((AppDbContext dataContext) => new AddressRepository(dataContext));
+        public ICityRepository Cities => 
+            GetOrCreateRepository((AppDbContext dataContext) => new CityRepository(dataContext));
+
+        public ICountryRepository Countries => 
+            GetOrCreateRepository((AppDbContext dataContext) => new CountryRepository(dataContext));
+        public IHourlyRateRepository HourlyRates => 
+            GetOrCreateRepository((AppDbContext dataContext) => new HourlyRateRepository(dataContext));
+        public IIdentificationRepository Identifications => 
+            GetOrCreateRepository((AppDbContext dataContext) => new IdentificationRepository(dataContext));
+        public IIdentificationTypeRepository IdentificationTypes => 
+            GetOrCreateRepository((AppDbContext dataContext) => new IdentificationTypeRepository(dataContext));
+        public IInvoiceRepository Invoices => 
+            GetOrCreateRepository((AppDbContext dataContext) => new InvoiceRepository(dataContext));
+        public IInvoiceLineRepository InvoiceLines => 
+            GetOrCreateRepository((AppDbContext dataContext) => new InvoiceLineRepository(dataContext));
+        public IMeansOfPaymentRepository MeansOfPayments => 
+            GetOrCreateRepository((AppDbContext dataContext) => new MeansOfPaymentRepository(dataContext));
+        public IPaymentRepository Payments => 
+            GetOrCreateRepository((AppDbContext dataContext) => new PaymentRepository(dataContext));
+        public IPriceListRepository PriceLists => 
+            GetOrCreateRepository((AppDbContext dataContext) => new PriceListRepository(dataContext));
+        public IPriceRepository Prices => 
+            GetOrCreateRepository((AppDbContext dataContext) => new PriceRepository(dataContext));
+        public IReviewRepository Reviews => 
+            GetOrCreateRepository((AppDbContext dataContext) => new ReviewRepository(dataContext));
+        public ISkillRepository Skills => 
+            GetOrCreateRepository((AppDbContext dataContext) => new SkillRepository(dataContext));
+        public ITaskRepository Tasks => 
+            GetOrCreateRepository((AppDbContext dataContext) => new TaskRepository(dataContext));
+        public ITaskTypeRepository TaskTypes => 
+            GetOrCreateRepository((AppDbContext dataContext) => new TaskTypeRepository(dataContext));
+        public IUserGroupRepository UserGroups => 
+            GetOrCreateRepository((AppDbContext dataContext) => new UserGroupRepository(dataContext));
+        public IUserInGroupRepository UserInGroups => 
+            GetOrCreateRepository((AppDbContext dataContext) => new UserInGroupRepository(dataContext));
+        public IUserOnAddressRepository UserOnAddresses => 
+            GetOrCreateRepository((AppDbContext dataContext) => new UserOnAddressRepository(dataContext));
+        public IUserOnTaskRepository UserOnTasks => 
+            GetOrCreateRepository((AppDbContext dataContext) => new UserOnTaskRepository(dataContext));
+        public IUserRepository Users => 
+            GetOrCreateRepository((AppDbContext dataContext) => new UserRepository(dataContext));
+        public IUserSkillRepository UserSills => 
+            GetOrCreateRepository((AppDbContext dataContext) => new UserSkillRepository(dataContext));
+        public IUserTypeRepository UserTypes => 
+            GetOrCreateRepository((AppDbContext dataContext) => new UserTypeRepository(dataContext));
+
+        
+        public IBaseRepository<TEntity> BaseRepository<TEntity>() where TEntity : class, new()
+        {
+            return GetOrCreateRepository((AppDbContext dataContext) => new BaseRepository<TEntity>(dataContext));
+        }
         
 
+        private TRepository GetOrCreateRepository<TRepository>(Func<AppDbContext, TRepository> factoryMethod)
+        {
+            _repositoryCache.TryGetValue(typeof(TRepository), out var repoObject);
+            if (repoObject != null)
+            {
+                return (TRepository) repoObject;
+            }
+
+            repoObject = factoryMethod(_appDbContext);
+            _repositoryCache[typeof(TRepository)] = repoObject;
+            return (TRepository) repoObject;
+        }
+        
         public AppUnitOfWork(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
         }
 
-        public int SaveChanges()
+        public virtual int SaveChanges()
         {
             return _appDbContext.SaveChanges();
         }
 
-        public async Task<int> SaveChangesAsync()
+        public virtual async Task<int> SaveChangesAsync()
         {
             return await _appDbContext.SaveChangesAsync();
-        }
-
-        public IBaseRepository<TEntity> BaseRepository<TEntity>() where TEntity : class, new()
-        {
-            return new BaseRepository<TEntity>(_appDbContext);
         }
 
     }
